50d18fe56b0794fb815d76f61f3751d47a562aef
Alexiuus <Alexis.centeno@mi.unc.edu.ar>
2023-02-24 19:50:42 -0300

    estructura del menú principal

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Rules from './components/Rules';
 import './Home.css';
-
-function Rules(){
-  return <div></div>;
-}
 
 function Home() {
     return (
-      <>
-        <div className="Home">
-          <div className="Menu-initGame container">
-            <h1 className='row offset-1'>3 class and a rat</h1>
-            <input className='row offset-1 col-6' type="text" placeholder="username"/>
-            <button className='row offset-1 col-4'>Game</button>
+      <div className='containerHome container-fluid'>
+        <div className="Home col-4">
+          <div className="Menu-initGame container text-center">
+            <div className='row offset-3 col-6'><h2 className='title'>3 class and a rat</h2></div>
+            <input className='username row offset-3 col-6' type="text" placeholder="username"/>
+            <button className='game row offset-4 col-4'  role="button"><span>Game</span></button>
           </div>
         </div>
-        <Rules/>
-      </>
+        <div className="Rules-home offset-5 col-6"><Rules/></div>
+      </div>
     );
 }
 
