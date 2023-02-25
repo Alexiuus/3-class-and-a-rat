823535f480f09b0bd42a5e3d270ae9667bf4b667
Alexiuus <Alexis.centeno@mi.unc.edu.ar>
2023-02-24 21:12:37 -0300

    manejo de errores

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
 import Rules from './components/Rules';
 import './Home.css';
 
+function Error({error}){
+  return <div className='error row offset-3 col-6'><span>{error}</span></div>
+}
+
 function Home() {
-    return (
-      <div className='containerHome container-fluid'>
-        <div className="Home col-4">
-          <div className="Menu-initGame container text-center">
-            <div className='row offset-3 col-6'><h2 className='title'>3 class and a rat</h2></div>
-            <input className='username row offset-3 col-6' type="text" placeholder="username"/>
-            <button className='game row offset-4 col-4'  role="button"><span>Game</span></button>
-          </div>
+  const [error, setError] = useState({ state: false,  error: 'init'});
+  const verifyUsername = (event) => {
+    if(!event.target.value) setError({state: true, error: 'empty name'});
+    else setError({ state: false,  error: ''});
+  };
+
+  const runGame = () => {
+    if (!error.state && error.error !== 'init') console.log("click");
+    else setError({state: true, error: 'empty name'});
+  }
+
+  return (
+    <div className='containerHome container-fluid'>
+      <div className="Home col-4">
+        <div className="Menu-initGame container text-center">
+          <div className='row offset-3 col-6'><h2 className='title'>3 class and a rat</h2></div>
+          <input className='username row offset-3 col-6' onChange={(e) => verifyUsername(e)} type="text" placeholder="username"/>
+          {
+           (error.state)? <Error error={error.error} /> : <div></div>  
+          }
+          <button className='game row offset-4 col-4' disabled={error.state} onClick={() => runGame()}><span>Game</span></button>
         </div>
-        <div className="Rules-home offset-5 col-6"><Rules/></div>
       </div>
-    );
+      <div className="Rules-home offset-5 col-6"><Rules/></div>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Home/components/Rules.jsx
+++ src/components/Home/components/Rules.jsx
@@ -2,24 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Rules.css';
 
 function Rules(){
-  return <div className='Rules container'>
+    return <div className='Rules container'>
         <h1 align='center'>Rules</h1>
         <p>
-        En general, los colores más comunes para los juegos de cartas son los tonos de rojo y azul. Estos son los colores tradicionales de los naipes y están profundamente arraigados en la cultura popular.
-
-Sin embargo, hay muchas variaciones y opciones disponibles para los juegos de cartas de póker. A continuación se presentan algunas opciones de color que se pueden considerar:
-
-    Rojo oscuro: el rojo oscuro es un color llamativo y clásico que se ve muy bien en los naipes de póker. Además, puede ser fácilmente combinado con otros colores para crear diseños atractivos.
-
-    Azul oscuro: el azul oscuro es otro color clásico que se utiliza a menudo en los naipes de póker. Es un color relajante y calmante que puede ayudar a mantener la atención durante largos períodos de tiempo.
-
-    Verde: el verde es un color que se asocia comúnmente con la suerte y la fortuna, lo que lo convierte en una buena opción para los naipes de póker. Además, puede ayudar a reducir la fatiga ocular después de largas sesiones de juego.
-
-    Negro: el negro es un color elegante y sofisticado que se utiliza a menudo en los naipes de póker de alta gama. Puede ser utilizado como un color secundario para resaltar los otros colores en el diseño.
-
-    Oro o plata: el oro y la plata son colores metálicos que pueden añadir un toque de lujo a los naipes de póker. Estos colores son ideales para juegos de cartas de alta calidad o para eventos especiales.
-
-En resumen, los colores perfectos para un juego de cartas tipo póker son una cuestión de preferencia personal y estilo de diseño. Los tonos de rojo y azul son los más comunes, pero otros colores como el verde, el negro y los tonos metálicos también pueden ser utilizados para crear naipes de póker atractivos y de alta calidad.
+            En general, los colores más comunes para los juegos de cartas son los tonos de rojo y azul. Estos son los colores tradicionales de los naipes y están profundamente arraigados en la cultura popular.
+            Sin embargo, hay muchas variaciones y opciones disponibles para los juegos de cartas de póker. A continuación se presentan algunas opciones de color que se pueden considerar:
+            Rojo oscuro: el rojo oscuro es un color llamativo y clásico que se ve muy bien en los naipes de póker. Además, puede ser fácilmente combinado con otros colores para crear diseños atractivos.
+            Azul oscuro: el azul oscuro es otro color clásico que se utiliza a menudo en los naipes de póker. Es un color relajante y calmante que puede ayudar a mantener la atención durante largos períodos de tiempo.
+            Verde: el verde es un color que se asocia comúnmente con la suerte y la fortuna, lo que lo convierte en una buena opción para los naipes de póker. Además, puede ayudar a reducir la fatiga ocular después de largas sesiones de juego.
+            Negro: el negro es un color elegante y sofisticado que se utiliza a menudo en los naipes de póker de alta gama. Puede ser utilizado como un color secundario para resaltar los otros colores en el diseño.
+            Oro o plata: el oro y la plata son colores metálicos que pueden añadir un toque de lujo a los naipes de póker. Estos colores son ideales para juegos de cartas de alta calidad o para eventos especiales.
+            En resumen, los colores perfectos para un juego de cartas tipo póker son una cuestión de preferencia personal y estilo de diseño. Los tonos de rojo y azul son los más comunes, pero otros colores como el verde, el negro y los tonos metálicos también pueden ser utilizados para crear naipes de póker atractivos y de alta calidad.
         </p>
     </div>;
 }
